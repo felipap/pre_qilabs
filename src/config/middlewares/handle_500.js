5eb5390dc2c592c8f673c4fa076b434d4fbe6b1d
f03lipe <pires.a.felipe@gmail.com>
2014-05-14 21:09:31 -0300

    permission:login now redirecting

--- src/config/middlewares/handle_500.js
+++ src/config/middlewares/handle_500.js
@@ -17,41 +17,37 @@ module.exports = function(err, req, res, next) {
 
 	var accept = req.headers.accept || '';
 
-	if (req.app.get('env') === 'production') {
-		if (~accept.indexOf('html')) {
-			res.render('pages/500', {
-				user: req.user,
-				message: err.human_message
-			});
-		} else {
-			var error = { message: err.message, stack: err.stack };
-			for (var prop in err) error[prop] = err[prop];
-			res
-				.set('Content-Type', 'application/json')
-				.end(JSON.stringify({ error: error }));
-		}
-	} else {
-		if (~accept.indexOf('html')) {
+	if (~accept.indexOf('html')) {
+		if (req.app.get('env') === 'production') {
 			if (err.permission = 'login') {
 				res.redirect('/');
 			}
 
 			res.render('pages/500', {
 				user: req.user,
+				message: err.human_message
+			});
+		} else {
+			res.render('pages/500', {
+				user: req.user,
 				error_code: res.statusCode,
 				error_msg: err,
 				error_stack: (err.stack || '').split('\n').slice(1).join('<br>'),
 			});
-		} else if (~accept.indexOf('json')) {
+		}
+	} else {
+		if (req.app.get('env') === 'production') {
+			var error = { message: err.message, stack: err.stack };
+			for (var prop in err) error[prop] = err[prop];
+			res
+				.set('Content-Type', 'application/json')
+				.end(JSON.stringify({ error: error }));
+		} else {	
 			var error = { message: err.message, stack: err.stack };
 			for (var prop in err) error[prop] = err[prop];
 			var json = JSON.stringify({ error: error });
 			res.setHeader('Content-Type', 'application/json');
 			res.end(json);
-		} else { // plain text
-			res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
-			res.end(err.stack);
 		}
 	}
-
 }
